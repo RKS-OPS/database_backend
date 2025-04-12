861ae4e7f891fc0db314e5166ef4599851608828
Raj K. Singh <raj.k.singh@ontario.ca>
2025-04-12 09:43:16 -0400

    searchIds

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,12 +6,14 @@ const userRoutes = require("./users");
 const locationRoutes = require("./locations");
 const organizationRoutes = require("./organizations");
 const dropdownsRoutes = require("./dropdowns");
+const projectsController = require("../controllers/projectsController"); //Added by raj April 12 2025
 
 router.get("/", (req, res) => {
   res.send("Hello from the /api endpoint!");
 });
 
 // Register Routes
+router.use("/projects/searchIds", projectsController.searchProjectIds); //Added by raj April 12 2025
 router.use("/projects", authenticateToken, projectRoutes);
 router.use("/users", userRoutes);
 router.use("/locations", locationRoutes);
